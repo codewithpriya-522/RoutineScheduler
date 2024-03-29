86e1091cb78c106ed9f7b9b697bac7190b989dd1
codewithpriya-522 <106175873+codewithpriya-522@users.noreply.github.com>
2024-03-29 12:42:04 +0530

    Add Tailwind CSS to RoutineSchedulerFrontend

---
+++ RoutineSchedulerFrontend/src/pages/First.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const First = () => {
+  return (
+    <div>
+        <h1 className='text-xl'>First Page</h1>
+        <p>This is the first page</p>
+        
+    </div>
+  )
+}
+
+export default First
